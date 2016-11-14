5a85cf58b3e754aafd6603839cd7bbe6d597776b
James <jamesdix@cwgsy.net>
2016-11-14 18:20:13 +0000

    maximum balance feature implemented"

--- lib/card.rb
+++ lib/card.rb
@@ -1,16 +1,18 @@
 class Card
 
-  attr_reader :balance
-
   DEFAULT_BALANCE = 0
+  MAXIMUM_BALANCE = 90
+
+  attr_reader :balance
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
 
   def top_up(amount)
-    @balance += amount 
-    "Top up successful"
+    raise "Top up failed: maximum balance of £#{MAXIMUM_BALANCE} reached" if @balance + amount > MAXIMUM_BALANCE 
+    @balance += amount
+    "Top up of £#{amount} successful. New balance: £#{@balance}"
   end
 
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -4,20 +4,30 @@ describe Card do
 
   subject(:card) { described_class.new }
 
+
   context "#balance" do
-    it "should return card balance as 0 for new card" do
+
+    before(:each) do
+      @amount = 90
+    end
+
+    it "should return card balance as £0 for new card" do
       expect(card.balance).to eq 0
     end
 
     it "checks that the card can be topped up" do
-      amount = 10
-      expect(card.top_up(amount)).to eq("Top up successful")
+      expect(card.top_up(@amount)).to eq("Top up of £#{@amount} successful. New balance: £#{card.balance}")
     end
 
     it 'checks the top up amount is added to the balance' do
-      amount = 10
-      card.top_up(amount)
-      expect(card.balance).to eq(10)
+      card.top_up(@amount)
+      expect(card.balance).to eq(90)
+    end
+
+    it 'should not allow user to have more than £90 on the card' do
+      maximum_balance =  Card::MAXIMUM_BALANCE
+      card.top_up(maximum_balance)
+      expect{card.top_up(2)}.to raise_error "Top up failed: maximum balance of £#{maximum_balance} reached"
     end
 
   end
