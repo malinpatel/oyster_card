f93ada44f6282931dbe0c6e687fcb4248a80392f
malin patel <malin.patel@icloud.com>
2016-11-15 10:43:15 +0000

    Touch in, touch out and in journey features added

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -3,7 +3,7 @@ class Oystercard
 
   MAXIMUM_BALANCE = 90
 
-  attr_reader :balance
+  attr_reader :balance, :in_use
 
   def initialize
     @balance = 0
@@ -20,6 +20,18 @@ class Oystercard
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def touch_in
+    @in_use = true
+  end
+
+  def touch_out
+    @in_use = false
+  end
+
+  def in_journey?
+    @in_use
   end
 
 end
