1160bebbfb9b60454e359916b36f410183ade251
keomony khun <keomony@gmail.com>
2016-11-15 15:42:51 +0000

    Section 5 : top_up method with 1 argument

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,10 @@ describe OysterCard do
   subject(:oystercard) {described_class.new}
 
   it { is_expected.to respond_to(:balance) }
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+  it "should increase the balance by top_up value" do
+    oystercard.top_up(10)
+    expect(oystercard.balance).to eq 10
+  end
+
 end
