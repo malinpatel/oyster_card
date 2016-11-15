99c01db9902ef5ec175a62f4830d8b6bd55cb7dd
keomony khun <keomony@gmail.com>
2016-11-15 12:13:44 +0000

    section 3 : debugging completed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,13 @@
 describe Oystercard do
 
+#type of error:
+#`<top (required)>': uninitialized constant Oystercard (NameError)
+#file path:
+#spec/oystercard_spec.rb
+#line number:
+#1
+#reason for the error:
+#Oystercard class has not been defined yet
+#solution:
+#define Oystercard class
 end
